4154a9342d25139c06bf4900e42d65551f26924c
Achal Singhal <achals2002@gmail.com>
2021-12-27 16:09:28 +0530

    Day 2 part one solved

---
+++ Day 2/__init__.py
@@ -0,0 +1,22 @@
+def calculate_pos(commands):
+    x = 0
+    depth = 0
+    for command in commands:
+        if command[0] == "forward":
+            x += command[1]
+        elif command[0] == "up":
+            depth -= command[1]
+        elif command[0] == "down":
+            depth += command[1]
+    return (x * depth)
+
+if __name__ == "__main__":
+    f = open('Day 2/input.txt', 'r')
+    commands_unparsed = (f.read()).splitlines()
+    commands = []
+        
+    for command in commands_unparsed:
+        command_split = command.split(" ")
+        commands.append([command_split[0], int(command_split[1])])
+    
+    print(calculate_pos(commands))
