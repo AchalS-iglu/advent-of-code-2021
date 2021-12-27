7a824398df8bc80b3f7cbac55723dd3c6b500bf4
Achal Singhal <achals2002@gmail.com>
2021-12-27 16:12:37 +0530

    Day 2 Solved

--- Day 2/__init__.py
+++ Day 2/__init__.py
@@ -10,6 +10,21 @@ def calculate_pos(commands):
             depth += command[1]
     return (x * depth)
 
+def calculate_pos_fixed(commands):
+    x = 0
+    depth = 0
+    aim = 0
+    for command in commands:
+        if command[0] == "forward":
+            x += command[1]
+            depth += (aim * command[1])
+        elif command[0] == "up":
+            aim -= command[1]
+        elif command[0] == "down":
+            aim += command[1]
+    return (x * depth)
+
+
 if __name__ == "__main__":
     f = open('Day 2/input.txt', 'r')
     commands_unparsed = (f.read()).splitlines()
@@ -19,4 +34,4 @@ if __name__ == "__main__":
         command_split = command.split(" ")
         commands.append([command_split[0], int(command_split[1])])
     
-    print(calculate_pos(commands))
+    print(calculate_pos_fixed(commands))
